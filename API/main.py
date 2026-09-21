b60b7d62b2048127760a059d331bd703753448cf
joaomarcosbgn <joaomarcos0916@gmail.com>
2026-09-21 11:23:44 -0300

    integrei front - API - banco de dados

--- API/main.py
+++ API/main.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 import models
 import schemas
 import crud
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, get_db
 from security import verificar_senha
 
@@ -14,6 +15,13 @@ app = FastAPI(
     title="Reparaê API",
     description="API de cadastro de usuários do Reparaê",
     version="1.0.0",
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
